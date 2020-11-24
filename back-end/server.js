6438b1d90405c2b5dc6a182cafdc9d5d60413557
James Camacho <camacho.james@gmail.com>
2020-11-23 17:19:35 -0700

    Finished the lab, need to add footer?

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -9,10 +18,92 @@ app.use(bodyParser.urlencoded({
 
 const mongoose = require('mongoose');
 
-// connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
-  useNewUrlParser: true
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  path: String,
 });
 
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/museum', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    description: req.body.description,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Delete an item.
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) })
+    console.log("Deleted " + req.params.id)
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    item = await Item.findOne({ _id: mongoose.Types.ObjectId(req.params.id) });
+    console.log("Editing " + item._id);
+    if (item.title !== req.body.title) {
+      console.log("Changing title from " + item.title + " to " + req.body.title);
+    }
+    if (item.description !== req.body.description) {
+      console.log("Changing description from " + item.description + " to " + req.body.description);
+    }
+    item.title = req.body.title;
+    item.description = req.body.description;
+    item.save();
+    console.log("Done!");
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
